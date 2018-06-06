147cf0c41b6c70c190466fcd91f4d2124ef6a4e8
jordyortega <jordy-9999@hotmail.com>
2018-06-06 09:57:54 +0200

    VAN ALLES GEK

---
+++ Wagenpark/Controllers/KostenController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Wagenpark.Models;
+
+namespace Wagenpark.Controllers
+{
+    public class KostenController : Controller
+    {
+
+        private WagenparkDBEntities db = new WagenparkDBEntities();
+
+        // GET: Kosten
+        public ActionResult Index()
+        {
+            DateTime dt = DateTime.Now.AddMonths(-6);
+            if (this.User.IsInRole("Admin"))
+            {
+                var oNDERHOUD = db.ONDERHOUD;
+                return View(oNDERHOUD.ToList());
+            }
+            else
+            {
+                var oNDERHOUD = db.ONDERHOUD.Where(o => o.CAR.DEALER.Naam == User.Identity.Name).Where(o => dt > o.OnderhoudsDatum );
+                return View(oNDERHOUD.ToList());
+            }
+        }
+    }
+}
